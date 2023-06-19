4ca7f922f35abc8ac0c1789d8dae535b95e54719
Sorami Hisamoto <hi@sorami.dev>
2023-06-19 14:45:36 +0900

    `base` の変更 - "/aist_3d_client/"

--- vite.config.ts
+++ vite.config.ts
@@ -31,7 +31,7 @@ import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "/aist_3d_client/",
   plugins: [
     vue(),
     viteStaticCopy({
